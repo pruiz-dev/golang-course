03213c52d69fdfec7a13c4236868b20912aba119
gituser <235141069+crocker203@users.noreply.github.com>
2026-08-20 14:25:46 -0400

    feat: add variable declaration and printing example to Go basics scratchpad

---
+++ 01_basics/02_variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const myName = "Dmitry"
+	var age int = 32
+	isLearningGo := true
+
+	fmt.Println(myName)
+	fmt.Println(age)
+	fmt.Println(isLearningGo)
+
+}
